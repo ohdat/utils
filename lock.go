c3af960f45bef52f313343657c89caf9331111e3
yyds <yanghao@ohdat.io>
2022-03-15 12:23:50 +0800

    增加分布式锁 使用方法

--- lock.go
+++ lock.go
@@ -9,6 +9,13 @@ import (
 const keyPrefix = "lock:"
 
 //GetLock 获取锁
+//how to use
+//lock := GetLock(db.RedisV8, fmt.Sprintf("ticket_balance_reduce:%d", id))
+//err = lock.Lock()
+//if err != nil {
+//	fmt.Println("xxx err",err)
+//}
+//defer  lock.Unlock()
 func GetLock(redisV8 *redis.Client, key string) (mutex *redsync.Mutex) {
 	pool := goredis.NewPool(redisV8)
 	rs := redsync.New(pool)
